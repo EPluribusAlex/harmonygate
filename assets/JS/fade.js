0d6640e46bae234e5d006bbaefd7c8ac57f8265a
Alexander Florian-Prescott <florianprescott@googlemail.com>
2019-01-14 10:57:50 -0500

    adding files

---
+++ assets/JS/fade.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+	const target = $(".main-row").offset().top;
+
+	let fade = true;
+
+  if($(window).width() >= 720) {
+  	$(window).scroll(function () {	
+
+      if($(window).scrollTop() >= target - 500 && fade) {
+      	fade = false;
+        $(".main-row").animate({"opacity": 1}, 300);
+        $(".plaque-6").animate({"opacity": 0}, 300);
+        $(".jumbotron").animate({"opacity": 0}, 300);
+
+      } else if($(window).scrollTop() < target - 500 && !fade) {
+      	fade = true;
+        $(".main-row").animate({"opacity": 0}, 300);
+        $(".plaque-6").animate({"opacity": 1}, 300);
+        $(".jumbotron").animate({"opacity": 1}, 300);
+      }
+
+  	});
+  }
+
+  else {
+    $(window).scroll(function () {  
+
+      if($(window).scrollTop() >= target - 400 && fade) {
+        fade = false;
+        $(".main-row").animate({"opacity": 1}, 300);
+        $(".plaque-6").animate({"opacity": 0}, 300);
+        $(".jumbotron").animate({"opacity": 0}, 300);
+
+      } else if($(window).scrollTop() < target - 400 && !fade) {
+        fade = true;
+        $(".main-row").animate({"opacity": 0}, 300);
+        $(".plaque-6").animate({"opacity": 1}, 300);
+        $(".jumbotron").animate({"opacity": 1}, 300);
+      }
+
+    });
+  }
+
+});
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# Harmony Gate website for Classical Acupuncture clinic in Durham
+```
+description
+```
+## Authors
+## License
+This project is licensed under the MIT License - see the LICENSE.md file for details
+## Acknowledgments
